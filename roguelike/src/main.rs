b60b46c98a8abc9680abde9516e8cdd4e7e7d6b2
David Horowitz <dhorowit@stevens.edu>
2019-04-21 01:16:15 -0400

    finished ch 1

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -3,9 +3,32 @@ extern crate tcod;
 use tcod::console::*;
 use tcod::colors;
 
+
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;
+
+
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        Key {code: Enter, alt: true, ..} => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+        Key {code: Escape, ..} => return true,  // exit game
+        Key {code: Up, ..} => *player_y -= 1,
+        Key {code: Down, ..} => *player_y += 1,
+        Key {code: Left, ..} => *player_x -= 1,
+        Key {code: Right, ..} => *player_x += 1,
+        _ => {},
+    }
+    false
+}
 
 fn main() {
     let mut root = Root::initializer()
@@ -15,13 +38,24 @@ fn main() {
     .title("Rust/libtcod tutorial")
     .init();
 
+    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !root.window_closed() {
-        root.set_default_foreground(colors::WHITE);
+        con.set_default_foreground(colors::WHITE);
         root.clear();
-        root.put_char(1, 1, '@', BackgroundFlag::None);
+        con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
         root.flush();
-        root.wait_for_keypress(true);
+        
+        con.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break
+        }
     }
 }
