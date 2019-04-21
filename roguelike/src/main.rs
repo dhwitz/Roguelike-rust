26af1383b5180374baa66187121d6991f34ac0c3
David Horowitz <dhorowit@stevens.edu>
2019-04-21 14:53:34 -0400

    right before rendom generation

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -1,15 +1,163 @@
 extern crate tcod;
+extern crate rand;
+
+use rand::Rng;
 
 use tcod::console::*;
-use tcod::colors;
+use tcod::colors::{self, Color};
 
+use std::cmp;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
 
-fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
+
+#[derive(Debug)]
+struct Object {
+    //main game object
+    x: i32,
+    y: i32,
+    chr: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, chr: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            chr: chr,
+            color: color
+        }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if !map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
+    }
+
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.chr, BackgroundFlag::None)
+    }
+
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    //allows creation of walls 
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile{blocked: false, block_sight: false}
+    }
+
+    pub fn wall() -> Self {
+        Tile{blocked: true, block_sight: true}
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect { x1: x, y1: y, x2: x + w, y2: y + h }
+    }
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        // returns true if this rectangle intersects with another one
+        (self.x1 <= other.x2) && (self.x2 >= other.x1) &&
+            (self.y1 <= other.y2) && (self.y2 >= other.y1)
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
+fn make_map() -> Map {
+    //fill map
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+
+    create_h_tunnel(25, 55, 23, &mut map);
+    map
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    //draw all objects
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    for object in objects {
+        object.draw(con);
+    }
+
+    blit(con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), root, (0, 0), 1.0, 1.0);
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
+    //handles keypresses
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -21,10 +169,10 @@ fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool 
             root.set_fullscreen(!fullscreen);
         }
         Key {code: Escape, ..} => return true,  // exit game
-        Key {code: Up, ..} => *player_y -= 1,
-        Key {code: Down, ..} => *player_y += 1,
-        Key {code: Left, ..} => *player_x -= 1,
-        Key {code: Right, ..} => *player_x += 1,
+        Key {code: Up, ..} => player.move_by(0, -1, map),
+        Key {code: Down, ..} => player.move_by(0, 1, map),
+        Key {code: Left, ..} => player.move_by(-1, 0, map),
+        Key {code: Right, ..} => player.move_by(1, 0, map),
         _ => {},
     }
     false
@@ -42,18 +190,26 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+
+    let player = Object::new(25, 23, '@', colors::WHITE);
+
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+
+    let map = make_map();
+
+    let mut objects = [player, npc];
 
     while !root.window_closed() {
-        con.set_default_foreground(colors::WHITE);
-        root.clear();
-        con.put_char(player_x, player_y, '@', BackgroundFlag::None);
-        blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
+        render_all(&mut root, &mut con, &mut objects, &map);
+
         root.flush();
         
-        con.put_char(player_x, player_y, ' ', BackgroundFlag::None);
-        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        for object in &objects {
+            object.clear(&mut con)
+        }
+
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player, &map);
         if exit {
             break
         }
